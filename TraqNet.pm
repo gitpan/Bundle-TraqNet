98de5a322566a6eac034aed728d162a063ec5d52
Jay Jacobs <jayj@cpan.org>
2002-02-01 23:12:12 +0000

    Import of JAYJ/Bundle-TraqNet-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-TraqNet
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         JAYJ/Bundle-TraqNet-1.00.tar.gz
    gitpan-cpan-author:       JAYJ
    gitpan-cpan-maturity:     released

---
+++ TraqNet.pm
@@ -0,0 +1,42 @@
+package Bundle::TraqNet;
+
+$VERSION = '1.00';
+
+1;
+__END__
+
+=head1 NAME
+
+Bundle::TraqNet - Bundled Modules for use in TraqNet Application
+
+=head1 SYNOPSIS
+
+
+C<perl -MCPAN -e 'install Bundle::TraqNet'>
+
+=head1 CONTENTS
+
+Apache::Cookie		- Part of libapreq, Jim Winstead 
+Apache::Request		- Part of libapreq, Jim Winstead 
+Apache::Session		- Session managment, Jeffrey Baker
+Date::Manip		- makes working with dates more fun then accounting systems - Sullivan Beck
+IO::File		- Graham Barr 
+MIME::Base64		- Gisle Aas 
+MIME::Entity		- Eryq
+Mail::IMAPClient	- David J. Kernen
+MIME::Types		- Mark Overmeer 
+Mail::Internet		- Mark Overmeer again
+String::Approx		- Jarkko Hietaniemi... thanks for being fuzzy
+Text::CSV_XS		- Jochen Wiedmann
+
+
+=head1 DESCRIPTION
+
+This bundle installs a whole bunch of perl modules which are all used by TraqNet.
+A system developed by LachNet Inc.  Much thanks to the folks who wrote these modules.
+
+=head1 AUTHOR
+
+Jay Jacobs E<lt>jay@lach.net<gt>
+
+=cut
